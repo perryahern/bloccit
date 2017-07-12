75bc8ade3f3dbcbb0bb4bc0e0d23f57f9262c888
Perry Ahern <pahern@gmail.com>
2017-07-12 02:01:47 -0400

    Ruby Foundations 22 assignment completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,19 @@ posts = Post.all
   )
 end
 
+# Create Sponsored Posts
+30.times do
+  SponsoredPost.create!(
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph,
+    price: RandomData.random_price
+  )
+end
+sponsored_posts = SponsoredPost.all
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored posts created"
