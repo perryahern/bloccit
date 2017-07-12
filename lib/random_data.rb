eaabf4d4ffe9b3b6ffd5959f06fc0ff5e8274c0c
Perry Ahern <pahern@gmail.com>
2017-07-11 22:39:04 -0400

    RF 22 assignment in progress

--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,26 +1,30 @@
 module RandomData
     def self.random_paragraph
-        sentences = []
-        rand(4..6).times do
-            sentences << random_sentence
-        end
+      sentences = []
+      rand(4..6).times do
+        sentences << random_sentence
+      end
 
-        sentences.join(" ")
+      sentences.join(" ")
     end
 
     def self.random_sentence
-        strings = []
-        rand(3..8).times do
-            strings << random_word
-        end
+      strings = []
+      rand(3..8).times do
+        strings << random_word
+      end
 
-        sentence = strings.join(" ")
-        sentence.capitalize << "."
+      sentence = strings.join(" ")
+      sentence.capitalize << "."
     end
 
     def self.random_word
-        letters = ('a'..'z').to_a
-        letters.shuffle!
-        letters[0,rand(3..8)].join
+      letters = ('a'..'z').to_a
+      letters.shuffle!
+      letters[0,rand(3..8)].join
+    end
+
+    def self.random_price
+      price = rand(100..250)
     end
 end
