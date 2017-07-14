da66c81bf7ed04bcdb6335d84b6a04f4de6959b5
Perry Ahern <pahern@gmail.com>
2017-07-14 16:02:17 -0400

    Ruby Foundations checkpoint 28 assignment completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ require 'random_data'
 # Create Users
 5.times do
   User.create!(
-# #3
   name:     RandomData.random_name,
   email:    RandomData.random_email,
   password: RandomData.random_sentence
@@ -45,6 +44,14 @@ admin = User.create!(
   email:    'admin@example.com',
   password: 'helloworld',
   role:     'admin'
+)
+
+# Create a moderator user
+moderator = User.create!(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
 )
 
 # Create a member
