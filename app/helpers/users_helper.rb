1b73093e07d26fb9f3cbd5f7960aaaca8f32c24b
Perry Ahern <pahern@gmail.com>
2017-07-20 18:44:54 -0400

    RF 32 part 2 completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  def display_posts
+    @user.posts.count > 0 ? "Posts" : "#{@user.name.split[0]} has made no posts yet."
+  end
+
+  def display_comments
+    @user.comments.count > 0 ? "Comments" : "#{@user.name.split[0]} has made no comments yet."
+  end
 end
