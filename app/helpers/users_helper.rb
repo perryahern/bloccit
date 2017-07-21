5072463d4afb1496c960093ac147c9db953325c9
Perry Ahern <pahern@gmail.com>
2017-07-21 10:19:47 -0400

    Ruby Foundations 32 assignment completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,14 @@
 module UsersHelper
 
   def display_posts
-    @user.posts.count > 0 ? "Posts" : "#{@user.name.split[0]} has made no posts yet."
+    @user.posts.count > 0 ? pluralize(@user.posts.count, 'Post') : "#{@user.name.split[0]} has made no posts yet."
   end
 
   def display_comments
-    @user.comments.count > 0 ? "Comments" : "#{@user.name.split[0]} has made no comments yet."
+    @user.comments.count > 0 ? pluralize(@user.comments.count, 'Comment') : "#{@user.name.split[0]} has made no comments yet."
+  end
+
+  def display_favorites
+    @user.favorites.count > 0 ? pluralize(@user.favorites.count, "Favorite") : "#{@user.name.split[0]} has favorited no posts yet."
   end
 end
